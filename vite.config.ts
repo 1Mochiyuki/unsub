4dd86a4387f8bfb9588bb3a6ca457f1248c240b0
1Mochiyuki <notsmile@protonmail.com>
2026-02-14 22:59:38 -0500

    im stupid

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,33 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
-import { devtools } from "@tanstack/devtools-vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import viteReact from "@vitejs/plugin-react";
-import viteTsConfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import viteReact from '@vitejs/plugin-react'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
 
-import tailwindcss from "@tailwindcss/vite";
-import { nitro } from "nitro/vite";
+import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
-    plugins: [
-        viteTsConfigPaths({
-            projects: ["./tsconfig.json"],
-        }),
-        tanstackStart(),
-        nitro(),
-        tailwindcss(),
-        viteReact({
-            babel: {
-                plugins: ["babel-plugin-react-compiler"],
-            },
-        }),
-        devtools(),
-    ],
-    build: {
-        rollupOptions: {
-            external: ["@tanstack/react-virtual"],
-        },
+  plugins: [
+    viteTsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
+    tanstackStart(),
+    nitro(),
+    tailwindcss(),
+    viteReact({
+      babel: {
+        plugins: ['babel-plugin-react-compiler'],
+      },
+    }),
+    devtools(),
+  ],
+  build: {
+    rollupOptions: {
+      external: [],
     },
-});
+  },
+})
 
-export default config;
+export default config
